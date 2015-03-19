cb71c3247955b6ec89704fdd28eeefb5de1ccaec
Synchunk <synchunk@web.de>
2015-03-19 16:27:56 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lantern for Minecraft</title>
+	</head>
+	<body>
+		<h1>Lantern for Minecraft</h1>
+		<p>We are still working on this website.</p>
+	</body>
+</html>
