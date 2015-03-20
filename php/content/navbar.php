805aa0c178718f1c947763fefc56ae7afc948dcc
Synchunk <synchunk@web.de>
2015-03-21 00:33:44 +0100

    Add logo to navbar

--- php/content/navbar.php
+++ php/content/navbar.php
@@ -1,5 +1,15 @@
+<?php
+	if (!defined('ROOT_PATH'))
+		exit();
+?>
 <nav class="navbar">
 	<ul>
+		<li class="navlogo">
+			<a href="<?php echo ROOT_PATH ?>/">
+				<img src="<?php echo ROOT_PATH ?>/img/logo.png" alt="Logo">
+				Lantern
+			</a>
+		</li>
 		<li><a href="#">One</a></li>
 		<li><a href="#">Two</a></li>
 		<li><a href="#">Three</a></li>
