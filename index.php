419edbfe0acd3a33e754cc0023991391ace22f5e
Synchunk <synchunk@web.de>
2015-03-21 14:26:02 +0100

    Add help section to front page

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
 			<h1>Lantern for Minecraft</h1>
 			<p>We are still working on this website.</p>
 		</div>
+		<div class="help-intro container">
+			<h1>Lantern for Minecraft</h1>
+			<p>We are still working on this website.</p>
+		</div>
 	</body>
 </html>
