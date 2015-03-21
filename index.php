4847f4b732999892260f0d645134fb944a71444b
Synchunk <synchunk@web.de>
2015-03-21 15:55:02 +0100

    Add beta warning to front page

--- index.php
+++ index.php
@@ -8,10 +8,15 @@
 		<?php require(__DIR__ . '/php/content/navbar.php'); ?>
 		<div class="intro container">
 			<h1>Lantern for Minecraft</h1>
-			<p>We are still working on this website.</p>
+			<p>Please use the form below if you need help.</p>
+			<div class="warning alert">
+				<strong>Warning!</strong>
+				We are still working on this website.
+				Use it at your own risk.
+			</div>
 		</div>
 		<div class="help-intro container">
-			<h1>Lantern for Minecraft</h1>
+			<h2>How can we help?</h2>
 			<p>We are still working on this website.</p>
 		</div>
 		<?php require(__DIR__ . '/php/content/footer.php'); ?>
--- php/content/footer.php
+++ php/content/footer.php
@@ -5,8 +5,6 @@
 <div class="footer">
 	<div class="container">
 		<ul>
-			<div class="left">
-			</div>
 			<div class="right">
 				<li class="disclaimer">This site is in no way affiliated with Minecraft or Mojang AB.</li>
 			</div>
